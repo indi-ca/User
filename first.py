245f4cbd8b82920898ec85c184669ae1d9ccad1a
Indika Piyasena <indika.piyasena@gmail.com>
2013-08-12 00:03:20 +1000

    autocommit

--- first.py
+++ first.py
@@ -1,5 +1,13 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, os
 
 class Bob(sublime_plugin.TextCommand):
     def run(self, edit):
         self.view.insert(edit, 0, "LocateMe")
+
+class FilenameToClipboardCommand(sublime_plugin.TextCommand):
+   def run(self, edit):
+      sublime.set_clipboard(os.path.basename(self.view.file_name()))
+
+class PathToClipboardCommand(sublime_plugin.TextCommand):
+   def run(self, edit):
+      sublime.set_clipboard(self.view.file_name())
