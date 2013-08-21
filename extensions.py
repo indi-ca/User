ae8b807a8e9c9211ba9f1fca5971c5e94d8097e3
Indika Piyasena <indika.piyasena@gmail.com>
2013-08-21 23:41:38 +1000

    autocommit

--- extensions.py
+++ extensions.py
@@ -2,6 +2,8 @@ import sublime, sublime_plugin
 import os
 import sys
 
+sidebar_visible = False
+
 class Bob(sublime_plugin.TextCommand):
     def run(self, edit):
         self.view.insert(edit, 0, "LocateMe")
@@ -16,15 +18,25 @@ class PathToClipboardCommand(sublime_plugin.TextCommand):
 
 class MyChainedActionsCommand(sublime_plugin.WindowCommand):
    def run(self):
-      # print sys.platform
-      #print platform()
-      #print "hello"
       if sys.platform == "win32":
         self.window.run_command("open_file", {"file": "${packages}/User/Default (Windows).sublime-keymap"})
+        self.window.run_command("open_file", {"file": "/Users/indika/Library/Preferences/PyCharm20/keymaps/Win_Pycharm_Frictionless.xml"})
       else:
         self.window.run_command("open_file", {"file": "${packages}/User/Default (OSX).sublime-keymap"})
-      #self.window.run_command("reveal_in_side_bar")
-      #self.window.run_command("focus_side_bar")
-      self.window.run_command("open_file", {"file": "${packages}/User/first.py"})
+        self.window.run_command("open_file", {"file": "/Users/indika/Library/Preferences/PyCharm20/keymaps/OSX_Pycharm_Frictionless.xml"})
+      self.window.run_command("open_file", {"file": "${packages}/User/extensions.py"})
+
+
+class OpenSublimeExplorerCommand(sublime_plugin.WindowCommand):
+   def run(self):
+      # https://github.com/reu-res/ST2Assistant/blob/master/ST2Assistant.py
+      global sidebar_visible
+      if sidebar_visible:
+        self.window.run_command("toggle_side_bar")
+        sidebar_visible = False
+      else:
+        self.window.run_command("reveal_in_side_bar")
+        self.window.run_command("focus_side_bar")
+        sidebar_visible = True
 
 
