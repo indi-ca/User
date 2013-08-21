95ec954a2d1df4a383d406d5085c8feba665612c
Indika Piyasena <indika.piyasena@gmail.com>
2013-08-21 22:28:05 +1000

    sublime extensions renamed

--- extensions.py
+++ extensions.py
@@ -1,4 +1,6 @@
-import sublime, sublime_plugin, os
+import sublime, sublime_plugin
+import os
+import sys
 
 class Bob(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -14,10 +16,15 @@ class PathToClipboardCommand(sublime_plugin.TextCommand):
 
 class MyChainedActionsCommand(sublime_plugin.WindowCommand):
    def run(self):
+      # print sys.platform
+      #print platform()
+      #print "hello"
+      if sys.platform == "win32":
+        self.window.run_command("open_file", {"file": "${packages}/User/Default (Windows).sublime-keymap"})
+      else:
+        self.window.run_command("open_file", {"file": "${packages}/User/Default (OSX).sublime-keymap"})
       #self.window.run_command("reveal_in_side_bar")
       #self.window.run_command("focus_side_bar")
-      self.window.run_command("open_file", {"file": "${packages}/User/Default (Windows).sublime-keymap"})
-      self.window.run_command("open_file", {"file": "${packages}/User/Default (OSX).sublime-keymap"})
       self.window.run_command("open_file", {"file": "${packages}/User/first.py"})
 
 
