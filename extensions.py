47ad68292578b5cc6684a14d02f54002baa22ead
Indika Piyasena <indika.piyasena@gmail.com>
2014-04-05 19:13:36 +1000

    removed some pushbullet stuff

--- extensions.py
+++ extensions.py
@@ -2,7 +2,11 @@ import sublime, sublime_plugin
 import os
 import sys
 
+
+
+
 sidebar_visible = True
+
 
 class InsertDateCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -11,37 +15,40 @@ class InsertDateCommand(sublime_plugin.TextCommand):
         #lineContents = 'Today' + '\n'
         #edit.insert(line.begin(), lineContents)
 
+
 class FilenameToClipboardCommand(sublime_plugin.TextCommand):
-   def run(self, edit):
-      sublime.set_clipboard(os.path.basename(self.view.file_name()))
+    def run(self, edit):
+        sublime.set_clipboard(os.path.basename(self.view.file_name()))
+
 
 class PathToClipboardCommand(sublime_plugin.TextCommand):
-   def run(self, edit):
-      sublime.set_clipboard(self.view.file_name())
+    def run(self, edit):
+        sublime.set_clipboard(self.view.file_name())
+
 
 class MyChainedActionsCommand(sublime_plugin.WindowCommand):
-   def run(self):
-      if sys.platform == "win32":
-        self.window.run_command("open_file", {"file": "${packages}/User/Default (Windows).sublime-keymap"})
-        # self.window.run_command("open_file", {"file": "U:/.PyCharm20/config/keymaps/Win_Pycharm_Frictionless.xml"})
-      else:
-        self.window.run_command("open_file", {"file": "${packages}/User/Default (OSX).sublime-keymap"})
-        self.window.run_command("open_file", {"file": "/Users/indika/Library/Preferences/PyCharm20/keymaps/OSX_Pycharm_Frictionless.xml"})
+    def run(self):
+        if sys.platform == "win32":
+            self.window.run_command("open_file", {
+                "file": "${packages}/User/Default (Windows).sublime-keymap"})
+            # self.window.run_command("open_file", {"file": "U:/.PyCharm20/config/keymaps/Win_Pycharm_Frictionless.xml"})
+        else:
+            self.window.run_command("open_file", {
+                "file": "${packages}/User/Default (OSX).sublime-keymap"})
 
-      self.window.run_command("open_file", {"file": "${packages}/User/extensions.py"})
+        self.window.run_command("open_file",
+                                {"file": "${packages}/User/extensions.py"})
 
 
 class OpenSublimeExplorerCommand(sublime_plugin.WindowCommand):
-   def run(self):
-      # https://github.com/reu-res/ST2Assistant/blob/master/ST2Assistant.py
-      global sidebar_visible
-      if sidebar_visible:
-        self.window.run_command("toggle_side_bar")
-        sidebar_visible = False
-      else:
-        self.window.run_command("reveal_in_side_bar")
-        self.window.run_command("focus_side_bar")
-        sidebar_visible = True
-
-
+    def run(self):
+        # https://github.com/reu-res/ST2Assistant/blob/master/ST2Assistant.py
+        global sidebar_visible
+        if sidebar_visible:
+            self.window.run_command("toggle_side_bar")
+            sidebar_visible = False
+        else:
+            self.window.run_command("reveal_in_side_bar")
+            self.window.run_command("focus_side_bar")
+            sidebar_visible = True
 
